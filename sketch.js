c5164bf903b2bb2bdfca22ce1485c0448efa75a9
navdeep2008 <74094165+navdeep2008@users.noreply.github.com>
2021-02-20 14:52:12 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,36 +1,47 @@
-
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
-
-function preload()
-{
-	
-}
+var wall , bullet;
+var speed , weight;
 
 function setup() {
-	createCanvas(800, 700);
+  createCanvas(800,400);
 
+  thickness = random(22,83);
+  speed = random(223,321);
+  weight = random(30,52);
 
-	engine = Engine.create();
-	world = engine.world;
+  bullet = createSprite(50,200, thickness, height/2);
+  bullet.velocityX = speed;
+  bullet.shapeColor = "black";
 
-	//Create the Bodies Here.
-
-
-	Engine.run(engine);
-  
+  wall = createSprite(1200 , 200,60,height/2);
+  wall.shapeColor = "black";
 }
-
 
 function draw() {
-  rectMode(CENTER);
-  background(0);
-  
-  drawSprites();
- 
+  background("black");  
+  var deformation = 0.5*bulletWeight*bulletSpeed*bulletSpeed/thicknessOfWall*thicknessOfWall*thicknessOfWall;
+
+  if(hasCollided(bullet , wall))
+{
+bullet.velocityX = 0;
+var damage = 0.5*weight*speed*speed / (thickness*thickness*thickness);  
 }
 
+if(damage > 10){
+wall.shapeColor = color(255,0,0);;  
+}
 
+if (damage < 10){
+wall.shapeColor = color(0.,2550); 
+}
 
+  drawSprites();
+}
+
+function hasCollided(bullet , wall){
+  bulletRightEdge = bullet.x + bullet.width;
+  wallLeftEdge = wall.x;
+  if(bulletRightEdge >= wallLeftEdge){
+  return true; 
+}
+  return false;
+}
